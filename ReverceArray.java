58085df6d1110d75175c9ffb149249a43aae8096
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-01-10 14:25:11 +0530

    Add files via upload

---
+++ ReverceArray.java
@@ -0,0 +1,16 @@
+package arrayExamples;
+
+public class ReverceArray {
+
+	public static void main(String[] args) {
+		
+		String name = "madam";
+		char[] ch = name.toCharArray();
+		for(int i=0;i<name.length();i++)
+		{
+			System.out.println(ch);
+		}
+		
+	}
+
+}
