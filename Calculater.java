88c11df608efee594ab83272889562f7217e256f
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-01-26 21:29:58 +0530

    Add files via upload

---
+++ Calculater.java
@@ -0,0 +1,14 @@
+package calculater;
+
+public class Calculater {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Addition: "+(10 + 20));
+		System.out.println("Substration: "+(20 - 20));
+		System.out.println("Multiflication: "+(10 * 20));
+		System.out.println("Division: "+(100 / 20));
+		System.out.println("Modulos: "+(100 % 20));
+	}
+
+}
