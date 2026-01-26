8e01709fe9b6f255b856f3afb5431b5d253a909d
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-01-26 21:29:22 +0530

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,14 @@
+package fifthProject;
+
+public class Hospital {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("-> Hospital Id: 001");
+		System.out.println("-> Hospital Name: Dava Seva");
+		System.out.println("-> Hospital Address: Pune");
+		System.out.println("-> Contact No: +91 8859364783");
+		System.out.println("-> Doctor Name: Dr. xyz");
+	}
+
+}
