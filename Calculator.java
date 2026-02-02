6903a7f491ec6f4bdbdcd53d139f5b764244721b
Vishal Kachare <155882477+Kacharevishal@users.noreply.github.com>
2026-02-02 14:53:07 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		int num1 = 50;
+		int num2 = 5;
+		
+		System.out.println("---> Following are the Arathmatic Operations...");
+		
+		System.out.println("\nAddition Of Two Number: "+ (num1)+" + "+(num2)+" = "+(num1 + num2));
+		System.out.println("Substraction Of Two Number: "+ (num1)+" - "+(num2)+" = "+(num1 - num2));
+		System.out.println("Multiflication Of Two Number: "+ (num1)+" * "+(num2)+" = "+(num1 * num2));
+		System.out.println("Division Of Two Number: "+ (num1)+" / "+(num2)+" = "+(num1 / num2));
+		System.out.println("Modulos Of Two Number: "+ (num1)+" % "+(num2)+" = "+(num1 % num2));
+	}
+
+}
